61823e272c133b7d6358b2db9c595d0685fe52ad
Thiago Ruis <contato@thiagoruis.com.br>
2021-04-30 19:28:21 -0300

    Added mongoengine

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 
+from mongoengine import connect
+
+from models import User
 from chat import Chat
 
 app = Flask(__name__)
+
+# Application SETUP
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+connect('chat_api', username='appUser', password='passwordForAppUser', authentication_source='admin')
 
 socketio.on_namespace(Chat('/'))
-
 
 if __name__ == '__main__':
     socketio.run(app)
