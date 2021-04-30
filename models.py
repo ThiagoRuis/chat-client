6d73bc6c52b1255952796ecf2b61c627cad3d3e5
Thiago Ruis <contato@thiagoruis.com.br>
2021-04-30 19:45:08 -0300

    Defined starting models

--- models.py
+++ models.py
@@ -1,7 +1,26 @@
-from mongoengine import Document, StringField
+from datetime import datetime
+from mongoengine import (
+    Document,
+    EmbeddedDocument,
+    StringField,
+    DateTimeField,
+    EmbeddedDocumentField,
+    ListField,
+)
 
 
-class User(Document):
+class User(EmbeddedDocument):
     email = StringField(required=True)
     first_name = StringField(max_length=50)
     last_name = StringField(max_length=50)
+
+
+class Message(Document):
+    text = StringField()
+    timestamp = DateTimeField(default=datetime.utcnow)
+    user = EmbeddedDocumentField(User)
+
+
+class Room(Document):
+    name = StringField(required=True)
+    users = ListField(EmbeddedDocumentField(User))
