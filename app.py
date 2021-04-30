dbdd293cc2f64debecf26d417a31ce4a23e6d5d1
Thiago Ruis <contato@thiagoruis.com.br>
2021-04-29 23:06:17 -0300

    added autopep8

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
 
+
 @socketio.on('send_message')
 def handle_message(data):
     print('received message: ' + data)
+
 
 if __name__ == '__main__':
     socketio.run(app)
