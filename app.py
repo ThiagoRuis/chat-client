f08fca9db74752f7b3294418da94e873940fc5cd
Thiago Ruis <contato@thiagoruis.com.br>
2021-04-29 23:03:52 -0300

    Added test endpoint and created html tester for the chat

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+from flask_socketio import SocketIO
 
-@app.route('/health-check')
-def health_check():
-    return 'API is working'
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, cors_allowed_origins="*")
+
+@socketio.on('send_message')
+def handle_message(data):
+    print('received message: ' + data)
+
+if __name__ == '__main__':
+    socketio.run(app)
