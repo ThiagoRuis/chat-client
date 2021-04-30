ef9fbdd698c78c3726dbf6b4f79ee44a1582e10f
Thiago Ruis <contato@thiagoruis.com.br>
2021-04-29 23:28:11 -0300

    saving sent messages, testing how reconnection works

--- chat.py
+++ chat.py
@@ -2,6 +2,15 @@ from flask_socketio import Namespace, emit
 
 
 class Chat(Namespace):
+    history = []
+    def on_connect(self):
+        emit('broadcast_message', self.history, broadcast=True)
+        print('user connected')
+
+    def on_disconnect(self):
+        print('user disconnected')
+
     def on_send_message(self, data):
         print('received message: ' + data)
+        self.history.append(data)
         emit('broadcast_message', data, broadcast=True)
