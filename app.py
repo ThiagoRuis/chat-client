cb4241e68051334cc6b04b59c1b2d27cba92a581
Thiago Ruis <contato@thiagoruis.com.br>
2021-04-29 23:11:21 -0300

    send_message event works receiving msg and emitting another event to the browser

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -9,6 +9,7 @@ socketio = SocketIO(app, cors_allowed_origins="*")
 @socketio.on('send_message')
 def handle_message(data):
     print('received message: ' + data)
+    emit('broadcast_message', data, broadcast=True)
 
 
 if __name__ == '__main__':
